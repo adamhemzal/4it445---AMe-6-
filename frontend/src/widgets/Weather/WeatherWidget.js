b4413075ceda6d7b57246f372ef0ec4fa74942ca
adamhemzal <adahemzal@gmail.com>
2017-11-02 14:55:18 +0100

    Zkouška s WeatherWidgetem

--- frontend/src/widgets/Weather/WeatherWidget.js
+++ frontend/src/widgets/Weather/WeatherWidget.js
@@ -6,19 +6,22 @@ export class WeatherWidget extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            cityName: ""
+            cityName: "",
+            weatherInfo: []
         };
     }
 
+    // Vzorec na převod kelvinů na stupně T(°C) = T(K) - 273.15
     // Jakmile se nahraje DOM pak se spustí tato metoda
     // Bacha - musíš použít data proměnnou !!
     componentDidMount() {
         axios.get('http://api.openweathermap.org/data/2.5/forecast?q=Prague,CZ&APPID=2bea6777b99c3b978671f4ab647b0948')
         .then(({data}) => {
             console.log(data);
-
+            console.log(data.list[0].weather)
             this.setState({
-                cityName: data.city.name
+                cityName: data.city.name,
+                weatherInfo: data.list[0].weather
             });
         })
         .catch((error) => {
@@ -27,8 +30,13 @@ export class WeatherWidget extends Component {
     }
 
     render() {
+        const weatherInfoDisplay = this.state.weatherInfo.map((currentWeather, index) => {
+            return <div key={index}> <p>{ currentWeather.main }</p> </div>
+        });
         return(
             <div>
+                <p>Mezera</p>
+                <p>{weatherInfoDisplay}</p>
                 <p>{this.state.cityName}</p>
             </div>
         )
