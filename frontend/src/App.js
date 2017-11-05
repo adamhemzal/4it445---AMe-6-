3904d737ed5e2a918dfdf9db12b619eb969fb7d8
Jan Budar <honza.budar@gmail.com>
2017-11-05 21:51:20 +0100

    WIdget TopAmers - implementace slideshow Slick.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
       <div className="App">
         <Dashboard />
       </div>
-      
+
     );
   }
 }
--- frontend/src/widgets/TopAmers/TopAmersWidget.js
+++ frontend/src/widgets/TopAmers/TopAmersWidget.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import AMer from '../../img/amer.png';
+import Slider from 'react-slick';
 
 export class TopAmersWidget extends Component {
 
@@ -17,7 +18,6 @@ export class TopAmersWidget extends Component {
     .then(response => {
       const { data: topAmers } = response;
       this.setState({ topAmers });
-      console.log(topAmers);
     })
     .catch(error => {
       console.log(error);
@@ -26,12 +26,29 @@ export class TopAmersWidget extends Component {
 
   render() {
     const { topAmers } = this.state;
-    console.log(topAmers.length > 0 && topAmers[0].image);
 
     if (topAmers.length === 0) {
       //TODO vyrenderovat nějakou dummy compomentu
-      return false;
+      return (
+        <div className="col-md-4 widget top_amers">
+          <div className="widget__inner widget__inner--dark">
+          </div>
+        </div>
+      )
     } else {
+      const settings = {
+        customPaging: i => {
+          return <div className="top_amers__navigation-item col"><div className="top_amers__navigation-item-inner"><img src={topAmers[i].image} /></div></div>
+        },
+        dots: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        dotsClass: "",
+        arrows: false
+      }
       return (
         <div className="col-md-4 widget top_amers">
           <div className="widget__inner widget__inner--dark">
@@ -43,69 +60,51 @@ export class TopAmersWidget extends Component {
             <div className="widget__content">
 
               <ul className="top_amers__list">
-                <li className="top_amers__list-item">
+                <Slider {...settings}>
+                  {topAmers.map((topAmer, index) =>
+                    // <li key={index} className="top_amers__navigation-item col">
+                    //   <div className="top_amers__navigation-item-inner">
+                    //     <a href="#"><img src={topAmer.image} alt={topAmer.realName} /></a>
+                    //   </div>
+                    // </li>
+                    <li className="top_amers__list-item">
 
-                  <div className="top_amers__image">
-                    <img src={topAmers[0].image} alt=""/>
-                    <span className="top_amers__badge">1.</span>
-                  </div>
+                      <div className="top_amers__image">
+                        <img src={topAmer.image} alt={topAmer.realName}/>
+                        <span className="top_amers__badge">{index + 1}.</span>
+                      </div>
 
-                  <div className="top_amers__content clearfix">
-                    <h3 className="top_amers__name float--left">{topAmers[0].realName}</h3>
-                    <h5 className="top_amers__value float--right">x{topAmers[0].ameCount}</h5>
-                  </div>
+                      <div className="top_amers__content clearfix">
+                        <h3 className="top_amers__name float--left">{topAmer.realName}</h3>
+                        <h5 className="top_amers__value float--right">x{topAmer.ameCount}</h5>
+                      </div>
+                    </li>
+                  )}
+                </Slider>
 
-                </li>
               </ul>
 
             </div>
 
-            <div className="widget__footer widget__footer--white text-center">
+            {/* <div className="widget__footer widget__footer--white text-center">
 
               <div className="top_amers__navigation clearfix">
                 <ul>
-                  {topAmers.map((topAmer, index) =>
-                    <li key={index} className="top_amers__navigation-item col">
-                      <div className="top_amers__navigation-item-inner">
-                        <a href="#"><img src={topAmer.image} alt={topAmer.realName} /></a>
-                      </div>
-                    </li>
-                  )}
-                  {/* <li className="top_amers__navigation-item col">
-                    <div className="top_amers__navigation-item-inner">
-                      <a href="#"><img src={AMer} alt="" /></a>
-                    </div>
-                  </li>
-                  <li className="top_amers__navigation-item col">
-                    <div className="top_amers__navigation-item-inner">
-                      <a href="#"><img src={AMer} alt=""/></a>
-                    </div>
-                  </li>
-                  <li className="top_amers__navigation-item col">
-                    <div className="top_amers__navigation-item-inner">
-                      <a href="#"><img src={AMer} alt="" /></a>
-                    </div>
-                  </li>
-                  <li className="top_amers__navigation-item col">
-                    <div className="top_amers__navigation-item-inner">
-                      <a href="#"><img src={AMer} alt="" /></a>
-                    </div>
-                  </li>
-                  <li className="top_amers__navigation-item col">
-                    <div className="top_amers__navigation-item-inner">
-                      <a href="#"><img src={AMer} alt="" /></a>
-                    </div>
-                  </li> */}
+                  <Slider {...settings}>
+                    {topAmers.map((topAmer, index) =>
+                      <li key={index} className="top_amers__navigation-item col">
+                        <div className="top_amers__navigation-item-inner">
+                          <a href="#"><img src={topAmer.image} alt={topAmer.realName} /></a>
+                        </div>
+                      </li>
+                    )}
+                  </Slider>
                 </ul>
               </div>
-            </div>
-
+            </div> */}
           </div>
-
         </div>
       )
     }
-
-
   }
 }
