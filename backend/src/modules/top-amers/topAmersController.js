be20630dbe6296c800c4ce62e2cfef61b94f5483
Jan Budar <honza.budar@gmail.com>
2017-11-05 20:20:16 +0100

    Implementace stahování zpráv pouze za poslední týden, změna channelu na #general.

--- backend/src/modules/top-amers/topAmersController.js
+++ backend/src/modules/top-amers/topAmersController.js
@@ -1,100 +1,103 @@
+const moment = require('moment');
 let sortedUsersWithAme = [];
 let globalRes = null;
 const topAmersMaxCount = 5;
 
 export const topAmersController = async (req, res) => {
 
-    const WebClient = require('@slack/client').WebClient;
-    const token = process.env.SLACK_API_TOKEN || '';
-    const web = new WebClient(token);
+  const WebClient = require('@slack/client').WebClient;
+  const token = process.env.SLACK_API_TOKEN || '';
+  const web = new WebClient(token);
+  //timestamp pro Slack - aktualni cas - jeden tyden
+  const oldestTimestamp = moment().subtract(1, 'week').format('X');
 
-    web.channels.history('C7693MGNS', {"count": 100}, (error, response) => {
-        if (error) {
-            console.log('Error:', error);
-        } else {
-            /*make res global so it is accessible from callback functions*/
-            globalRes = res;
+  web.channels.history('C0BUA20S0', {'count': 1000, 'oldest': oldestTimestamp}, (error, response) => {
+    if (error) {
+      console.log('Error:', error);
+    } else {
+      /*make res global so it is accessible from callback functions*/
+      globalRes = res;
 
-            let usersWithAme = {}; /*{userID : ameCount}*/
+      let usersWithAme = {}; /*{userID : ameCount}*/
 
-            /*Goes through the messages and selects users who are rated with at least one AMe*/
-            usersWithAme = getUsersWithAme(response.messages);
+      /*Goes through the messages and selects users who are rated with at least one AMe*/
+      usersWithAme = getUsersWithAme(response.messages);
 
-            /*Sorts the users by AMe count, result goes to global sortedUsersWithAme array*/
-            getSortedAmers(usersWithAme);
-            sortedUsersWithAme.slice(0, topAmersMaxCount);/*just selects the top N users*/
+      /*Sorts the users by AMe count, result goes to global sortedUsersWithAme array*/
+      getSortedAmers(usersWithAme);
+      sortedUsersWithAme.slice(0, topAmersMaxCount);/*just selects the top N users*/
 
-            /*pairs the users (IDs) with names and pictures, outputs them*/
-            getSlackUsers(sortedUsersWithAme);
-        }
-    });
+      /*pairs the users (IDs) with names and pictures, outputs them*/
+      getSlackUsers(sortedUsersWithAme);
+    }
+  });
 };
 
 const getUsersWithAme = (messages) => {
-    let usersWithAme = {};
-    messages.forEach((message) => {
-        const userID = message.user;
-        let reactions = message.reactions;
+  let usersWithAme = {};
+  messages.forEach((message) => {
+    const userID = message.user;
+    let reactions = message.reactions;
 
-        if (reactions !== undefined) {
+    if (reactions !== undefined) {
 
-            reactions.forEach((reaction) => {
-                if (reaction.name === "ame") {
-                    usersWithAme[userID] = usersWithAme[userID] ? usersWithAme[userID] + 1 : 1;
-                }
-            });
+      reactions.forEach((reaction) => {
+        if (reaction.name === "ame") {
+          usersWithAme[userID] = usersWithAme[userID] ? usersWithAme[userID] + 1 : 1;
         }
-    });
-    return usersWithAme;
+      });
+    }
+  });
+  return usersWithAme;
 };
 
 const getSortedAmers = (usersWithAme) => {
-    let tempSortedUsersWithAme = [];
-    /*sort the keys, returns Array*/
-    tempSortedUsersWithAme = Object.keys(usersWithAme).sort((a, b) => {
-        return usersWithAme[b] - usersWithAme[a];
-    });
+  let tempSortedUsersWithAme = [];
+  /*sort the keys, returns Array*/
+  tempSortedUsersWithAme = Object.keys(usersWithAme).sort((a, b) => {
+    return usersWithAme[b] - usersWithAme[a];
+  });
 
-    /*assign ame counts to the keys (were lost in during the sorting)*/
-    tempSortedUsersWithAme.forEach((userID) => {
-        sortedUsersWithAme[userID] = {ameCount: usersWithAme[userID], userID: userID};
-    });
+  /*assign ame counts to the keys (were lost in during the sorting)*/
+  tempSortedUsersWithAme.forEach((userID) => {
+    sortedUsersWithAme[userID] = {ameCount: usersWithAme[userID], userID: userID};
+  });
 };
 
 const getSlackUsers = () => {
-    const WebClient = require('@slack/client').WebClient;
-    const token = process.env.SLACK_API_TOKEN || '';
-    const web = new WebClient(token);
+  const WebClient = require('@slack/client').WebClient;
+  const token = process.env.SLACK_API_TOKEN || '';
+  const web = new WebClient(token);
 
-    web.users.list(getSlackUsersCallback);
+  web.users.list(getSlackUsersCallback);
 };
 
 const getSlackUsersCallback = (error, response) => {
-    let topAmers = [];
-    let i = 0;
+  let topAmers = [];
+  let i = 0;
 
-    if (error) {
-        console.log('Error:', error);
-    } else {
-        response.members.forEach((member) => {
-            if (sortedUsersWithAme[member.id]) {
-                sortedUsersWithAme[member.id]["realName"] = member.real_name;
-                sortedUsersWithAme[member.id]["image"] = member.profile.image_512;
-                topAmers.push(sortedUsersWithAme[member.id]);
-                i++;
-            }
-        });
+  if (error) {
+    console.log('Error:', error);
+  } else {
+    response.members.forEach((member) => {
+      if (sortedUsersWithAme[member.id]) {
+        sortedUsersWithAme[member.id]["realName"] = member.real_name;
+        sortedUsersWithAme[member.id]["image"] = member.profile.image_512;
+        topAmers.push(sortedUsersWithAme[member.id]);
+        i++;
+      }
+    });
 
-        topAmers = sortTopAmers(topAmers);/*sort once again by ame count*/
-    }
+    topAmers = sortTopAmers(topAmers);/*sort once again by ame count*/
+  }
 
-    /*output the topAmers, now into the console*/
-    globalRes.send(topAmers);
+  /*output the topAmers, now into the console*/
+  globalRes.send(topAmers);
 };
 
 const sortTopAmers = (topAmers) => {
-    topAmers.sort((a, b) => {
-        return a.ameCount < b.ameCount;
-    });
-    return topAmers;
+  topAmers.sort((a, b) => {
+    return a.ameCount < b.ameCount;
+  });
+  return topAmers;
 };
