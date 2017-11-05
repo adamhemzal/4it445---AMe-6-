2614eecf2d77da1c880eca02ff38f54e76e9ab53
adamhemzal <adahemzal@gmail.com>
2017-11-05 15:44:14 +0100

    Zprovozneni icony u pocasi

--- frontend/src/widgets/Weather/WeatherWidget.js
+++ frontend/src/widgets/Weather/WeatherWidget.js
@@ -35,27 +35,70 @@ export class WeatherWidget extends Component {
     }
 
     render() {
-
-        //Mapování icon na IDčka
-        const weatherIconMap = {
-            "01d" : "wi-day-sunny",
-            "01n" : "wi-night-clear",
-            "02d" : "wi-day-cloudy",
-            "02n" : "wi-night-alt-cloudy",
-            "03d" : "wi-cloud",
-            "03n" : "wi-cloud",
-            "04d" : "wi-cloudy",
-            "04n" : "",
-            "09d" : "wi-showers",
-            "09n" : "wi-showers",
-            "10d" : "wi-rain",
-            "10n" : "wi-rain",
-
-        }
         
         // Vypiš iconu k počasí
         const weatherIconDisplay = this.state.weatherIcon.map((currentWeather, index) => {
-            return <img key={index} src={`http://openweathermap.org/img/w/${currentWeather.icon}.png`} alt="" />
+            let weatherIconMap;
+            switch(currentWeather.icon) {
+                case "01d":
+                    weatherIconMap = "wi-day-sunny"
+                    break;
+                case "01n":
+                    weatherIconMap = "wi-night-clear"
+                    break;
+                case "02d":
+                    weatherIconMap = "wi-day-cloudy"
+                    break;
+                case "02n":
+                    weatherIconMap = "wi-night-alt-cloudy"
+                    break;
+                case "03d":
+                    weatherIconMap = "wi-cloud"
+                    break;
+                case "03n":
+                    weatherIconMap = "wi-cloud"
+                    break;
+                case "04d":
+                    weatherIconMap = "wi-cloudy"
+                    break;
+                case "04n":
+                    weatherIconMap = "wi-cloudy"
+                    break;
+                case "09d":
+                    weatherIconMap = "wi-showers"
+                    break;
+                case "09n":
+                    weatherIconMap = "wi-showers"
+                    break;
+                case "10d":
+                    weatherIconMap = "wi-rain"
+                    break;
+                case "10n":
+                    weatherIconMap = "wi-rain"
+                    break;
+                case "11d":
+                    weatherIconMap = "wi-thunderstorm"
+                    break;
+                case "11n":
+                    weatherIconMap = "wi-thunderstorm"
+                    break;
+                case "13d":
+                    weatherIconMap = "wi-snow"
+                    break;
+                case "13n":
+                    weatherIconMap = "wi-snow"
+                    break;
+                case "50d":
+                    weatherIconMap = "wi-fog"
+                    break;
+                case "50n":
+                    weatherIconMap = "wi-fog"
+                    break;
+                default:
+                    weatherIconMap = "Není k dispozici"
+            }
+            //<img key={index} src={`http://openweathermap.org/img/w/${currentWeather.icon}.png`} alt="" />
+            return <i className={`wi ${weatherIconMap}`}></i>
         });
 
         // Vypiš temperature v °C
@@ -84,11 +127,10 @@ export class WeatherWidget extends Component {
                         <div className="weather__slide">
 
                             <div className="weather__icon">
-                                {/* <i className="wi wi-night-sleet"></i> */}
                                 {weatherIconDisplay}
                             </div>
+                            
                             <h3 className="weather__city">{this.state.cityName}</h3>
-
                         </div>
 
                     </div>
