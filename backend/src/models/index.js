12cab31a186767d355bf831f074174dfd9958bb4
Jan Budar <honza.budar@gmail.com>
2017-11-16 14:28:40 +0100

    Nakonfigurování databáze a ORM Sequelize.

---
+++ backend/src/models/index.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+var Sequelize = require('sequelize');
+var basename = path.basename(__filename);
+var env = process.env.NODE_ENV || 'development';
+var config = require(__dirname + '/../config/db.js')[env];
+var DataTypes = require('sequelize/lib/data-types');
+var db = {};
+
+if (config.use_env_variable) {
+  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+} else {
+  var sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
+}
+
+// Remember to require all model files here:
+// const modelModules = [require('./product'), require('./productcategory')];
+//
+// modelModules.forEach(function(modelModule) {
+//   const model = modelModule(sequelize, DataTypes);
+//   db[model.name] = model;
+// });
+//
+// Object.keys(db).forEach(modelName => {
+//   if (db[modelName].associate) {
+//     db[modelName].associate(db);
+//   }
+// });
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
