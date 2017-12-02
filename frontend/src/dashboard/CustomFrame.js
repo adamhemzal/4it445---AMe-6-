c0e21707553222bfa294e0c69890cd7001f09964
Jan Budar <honza.budar@gmail.com>
2017-12-02 22:44:48 +0100

    Implementace otevírání modálního okna po kliknutí na edit u widgetu.

--- frontend/src/dashboard/CustomFrame.js
+++ frontend/src/dashboard/CustomFrame.js
@@ -1,26 +1,77 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Modal from 'react-modal';
 
-const CustomFrame = ({title, editable, children, onRemove }) => {
-  return (
-      <div className="widget__outer custom-frame-container">
-        <div className="title widget__edit">
-            {editable &&
-                    <div>
-                        <a className="btn" onClick={() => {editWidget();}} >Edit</a>
-                        <a className="btn" onClick={() => {onRemove();}} >Remove</a>
-                    </div>
-            }
-        </div>
-        <div className="custom-frame-content">
-          {children}
-        </div>
-    </div>
-  );
-};
+class CustomFrame extends Component {
 
-const editWidget = () => {
-    console.log(this);
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      modalIsOpen: false
+    };
+
+    this.openModal = this.openModal.bind(this);
+    this.afterOpenModal = this.afterOpenModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+
+  componentWillMount() {
+    Modal.setAppElement('#root');
+  }
+
+  editWidget = (children) => {
+    console.log(children.type.name);
     return;
-};
+  };
 
-export default CustomFrame;
+  openModal() {
+    this.setState({modalIsOpen: true});
+  }
+
+  afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    this.subtitle.style.color = '#f00';
+  }
+
+  closeModal() {
+    this.setState({modalIsOpen: false});
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="widget__outer custom-frame-container">
+          <div className="title widget__edit">
+            {this.props.editable &&
+              <div>
+                <a className="btn" onClick={() => {this.openModal();}} >Edit</a>
+                <a className="btn" onClick={() => {this.props.onRemove();}} >Remove</a>
+              </div>
+            }
+          </div>
+          <div className="custom-frame-content">
+            {this.props.children}
+          </div>
+        </div>
+        <div>
+          <Modal
+            isOpen={this.state.modalIsOpen}
+            onAfterOpen={this.afterOpenModal}
+            onRequestClose={this.closeModal}
+            contentLabel="Example Modal"
+            >
+
+              <h2 ref={subtitle => this.subtitle = subtitle}>Hello</h2>
+              <button onClick={this.closeModal}>close</button>
+              <div>I am a modal</div>
+            </Modal>
+          </div>
+        </div>
+      )
+    }
+  };
+
+
+
+  export default CustomFrame;
