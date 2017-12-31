45f22ed8fa147642d113829c13f4b9674b07f248
AntoninPrazsky <33231531+AntoninPrazsky@users.noreply.github.com>
2017-12-31 13:23:36 +0100

    Odstranění nepotřebného kódu

--- backend/src/modules/people-day/routes.js
+++
@@ -1,9 +0,0 @@
-import expressAsyncAwait from 'express-async-await';
-import { Router } from 'express';
-
-import { weatherController } from './weatherController.js';
-
-const router = expressAsyncAwait(Router());
-router.get('/', weatherController);
-
-export default router;
