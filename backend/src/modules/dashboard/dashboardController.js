6c5d9c44084fd59f479d3738e19b2099ec203894
Dan <Dan@10.0.0.36>
2018-01-13 22:24:43 +0100

    Pridavani dashboardu

--- backend/src/modules/dashboard/dashboardController.js
+++ backend/src/modules/dashboard/dashboardController.js
@@ -2,47 +2,78 @@ import db from '../../models/';
 
 export const saveDashboardController = async (req, res) => {
 
-  const { dashboardId, name, description, url, layoutId, layout } = req.body;
+    const {dashboardId, name, description, url, layoutId, layout} = req.body;
+    
+    if (!dashboardId || dashboardId === "") {
+        
+        let newDashboardId = generateNewId();
+        
+        db.dashboard.create({
+            id: newDashboardId,
+            name: name,
+            description: description,
+            url: url,
+            layout: layout,
+            layoutId: layoutId
+        }).then(result => {
+            res.json({success: true});
+        }).catch(err => {
+            res.json({success: false});
+        });
+    } else {
+        
+        db.dashboard.update(
+                {
+                    name: name,
+                    description: description,
+                    url: url,
+                    layout: layout,
+                    layoutId: layoutId
+                },
+                {where: {id: dashboardId}})
+                .then(result => {
+                    res.json({success: true});
+                }).catch(err => {
+            res.json({success: false});
+        });
+    }
+};
 
-  db.dashboard.update(
-    {
-      name: name,
-      description: description,
-      url: url,
-      layout: layout,
-      layoutId: layoutId
-    },
-    {where: {id: dashboardId}})
-    .then(result => {
-      res.json({success: true});
-    }).catch(err => {
-      res.json({success: false});
-    });
-  }
+export const getDashboardController = async (req, res) => {
 
-  export const getDashboardController = async (req, res) => {
-
-    const { dashboardId } = req.params;
+    const {dashboardId} = req.params;
 
     db.dashboard.findOne({where: {id: dashboardId}})
-    .then(dashboard => {
-      if (dashboard) {
-        res.json(
-          {
-            success: true,
-            id: dashboard.id,
-            name: dashboard.name,
-            description: dashboard.description,
-            url: dashboard.url,
-            layoutId: dashboard.layoutId,
-            layout: dashboard.layout,
-          }
-        )
-      } else {
+            .then(dashboard => {
+                if (dashboard) {
+                    res.json(
+                            {
+                                success: true,
+                                id: dashboard.id,
+                                name: dashboard.name,
+                                description: dashboard.description,
+                                url: dashboard.url,
+                                layoutId: dashboard.layoutId,
+                                layout: dashboard.layout,
+                            }
+                    )
+                } else {
+                    res.json({success: false});
+                }
+            }).catch(err => {
+        console.log(err);
         res.json({success: false});
-      }
-    }).catch(err => {
-      console.log(err);
-      res.json({success: false});
     });
-  }
+}
+
+
+const generateNewId = () => {
+    let text = "";
+    let possible = "0123456789";
+    let length = 6;
+
+    for (let i = 0; i < length; i++)
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+};
