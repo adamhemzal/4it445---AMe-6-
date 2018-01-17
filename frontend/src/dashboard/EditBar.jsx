48a19d8c4f31474fb0ede7a49b81a138881eff7e
Jan Kodeš <kodes.honza@gmail.com>
2018-01-17 10:37:42 +0100

    - CSS upravy a merge

--- frontend/src/dashboard/EditBar.jsx
+++ frontend/src/dashboard/EditBar.jsx
@@ -5,7 +5,7 @@ const EditBar = ({ onEdit }) => (
 	<div className="row edit-bar">
 		<div className="col-sm-12 text-right">
 			<button type="button" className="btn btn-default btn-xs" onClick={onEdit}>
-				<i className="fa fa-pencil-square-o" aria-hidden="true" /> Edit
+				<i className="fa fa-pencil-square-o" aria-hidden="true" /> Edit Widgets
 			</button>
 		</div>
 	</div>
