c4f0739eb0f252a787970ce5d79154525ebdc2fc
Dan <Dan@10.0.0.36>
2018-01-17 19:44:05 +0100

    Slack users getter

---
+++ backend/src/modules/slack-users/routes.js
@@ -0,0 +1,11 @@
+import expressAsyncAwait from 'express-async-await';
+import { Router } from 'express';
+
+import { getList } from './slackUsersController.js';
+import { getDetails } from './slackUsersController.js';
+
+const router = expressAsyncAwait(Router());
+router.get('/list', getList);
+router.get('/details', getDetails);
+
+export default router;
